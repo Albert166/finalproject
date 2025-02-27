261fd6ff5c8f7544d38086e084e06a79a30ab7ef
albert <albert.mackel@gmail.com>
2025-02-27 21:52:03 +0200

    updated ansible inventory to work with docker-compose-cloudwatch.yml

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,19 +2,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-# EC2 Instance
-resource "aws_instance" "shopping_list_instance" {
-  ami           = "ami-0a628e1e89aaedf80"
-  instance_type = var.instance_type
-  key_name = "shopping-list-key-pair"
-  vpc_security_group_ids = [aws_security_group.https.id, aws_security_group.ssh.id]
-  subnet_id = aws_subnet.main-b.id
-  associate_public_ip_address = true
-  tags = {
-    Name = "shopping-list-webapp-instance"
-  }
-}
-
 # VPC
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
@@ -112,3 +99,25 @@ resource "aws_route_table_association" "main" {
   subnet_id      = aws_subnet.main-b.id
   route_table_id = aws_route_table.main.id
 }
+
+# IAM Role
+resource "aws_iam_instance_profile" "ec2_cloudwatch_profile" {
+  name = "EC2_Cloudwatch_Profile"
+  role = "EC2_Cloudwatch"
+}
+
+
+# EC2 Instance
+resource "aws_instance" "shopping_list_instance" {
+  ami           = "ami-0a628e1e89aaedf80"
+  instance_type = var.instance_type
+  key_name = "shopping-list-key-pair"
+  vpc_security_group_ids = [aws_security_group.https.id, aws_security_group.ssh.id]
+  subnet_id = aws_subnet.main-b.id
+  associate_public_ip_address = true
+  iam_instance_profile = aws_iam_instance_profile.ec2_cloudwatch_profile.name
+  tags = {
+    Name = "shopping-list-webapp-instance"
+  }
+}
+
