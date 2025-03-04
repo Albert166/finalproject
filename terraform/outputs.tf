1e34f63bc33c286f0db473a39f39aff32c18589e
albert <albert.mackel@gmail.com>
2025-03-04 18:40:18 +0200

    updated outputs.tf file to show public dns

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "instance_ip" {
-  description = "The public IP address of the EC2 instance"
-  value       = aws_instance.shopping_list_instance.public_ip
+  description = "The public DNS address of the EC2 instance"
+  value       = aws_instance.shopping_list_instance.public_dns
 }
